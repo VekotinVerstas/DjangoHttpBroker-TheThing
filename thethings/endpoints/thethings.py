8ae6b246fc706b371754ea02a6e94fd3597164e9
Aapo Rista <aapo.gitpub@rista.net>
2019-05-15 16:38:19 +0300

    Sanitize handler

--- thethings/endpoints/thethings.py
+++ thethings/endpoints/thethings.py
@@ -17,17 +17,17 @@ class TheThingsEndpoint(EndpointProvider):
         if request.method != 'POST':
             return HttpResponse('Only POST with JSON body is allowed', status=405)
         serialised_request = serialize_django_request(request)
+        serialised_request['time'] = datetime.datetime.utcnow().isoformat() + 'Z'
         ok, body = decode_json_body(serialised_request['request.body'])
         if ok is False:
             return HttpResponse(f'JSON ERROR: {body}', status=400, content_type='text/plain')
         devid = body.get('hardware_serial')
         if devid is not None:
-            serialised_request['devid'] = devid
-            serialised_request['time'] = datetime.datetime.utcnow().isoformat() + 'Z'
-            message = data_pack(serialised_request)
-            key = create_routing_key('thethings', devid)
-            send_message(settings.RAW_HTTP_EXCHANGE, key, message)
             datalogger, created = get_datalogger(devid=devid, update_activity=True)
         else:
             devid = 'ttn-unknown'
+        serialised_request['devid'] = devid
+        message = data_pack(serialised_request)
+        key = create_routing_key('thethings', devid)
+        send_message(settings.RAW_HTTP_EXCHANGE, key, message)
         return HttpResponse('OK', content_type='text/plain')
